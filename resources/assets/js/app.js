75b9ebdfe001a0cb632d30156bb2a0ab71d541ec
Chris Browder <rcbrowder@gmail.com>
2018-07-20 15:59:20 -0400

    Oh no...

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,8 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueFlip from 'vue-flip'
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -20,3 +22,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
 const app = new Vue({
     el: '#app'
 });
+
+
+// VueFlip
+Vue.use(VueFlip);
