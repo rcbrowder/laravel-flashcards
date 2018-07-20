bbc719d4c1cb6179e36b03cf13f34a2195c9073a
Chris Browder <rcbrowder@gmail.com>
2018-07-20 10:55:52 -0400

    Fixes Login/Register controller redirects.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/card';
+    protected $redirectTo = '/cards';
 
     /**
      * Create a new controller instance.
