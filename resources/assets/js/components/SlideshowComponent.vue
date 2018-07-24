2496e0a5bddb00ccc859b2f3bf65bc18958d7349
Chris Browder <rcbrowder@gmail.com>
2018-07-24 14:01:51 -0400

    Created props, data, and computed attributes in SlideshowComponent.vue. Adds shuffle function to computed.

--- resources/assets/js/components/SlideshowComponent.vue
+++ resources/assets/js/components/SlideshowComponent.vue
@@ -5,5 +5,40 @@
 
 <script>
 
+    export default {
+
+        props: ['cardsData'],
+
+        data: () => ({
+            cardOrder: [],
+
+        }),
+
+        computed: {
+
+            /**
+            * Shuffles array in place.
+            * @param {Array} a items An array containing the items.
+            */
+
+            determineCardOrder: function() {
+
+                function shuffle(a) {
+                var j, x, i;
+                for (i = a.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                x = a[i];
+                a[i] = a[j];
+                a[j] = x;
+                }
+                return a;
+}
+            }
+        },
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
 
 </script>
