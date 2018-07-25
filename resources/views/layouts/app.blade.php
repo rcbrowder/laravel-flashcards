69664a7e07c138c7798063fb807085aff48b77a4
Chris Browder <rcbrowder@gmail.com>
2018-07-25 16:05:39 -0400

    Adds favicon to app layout so it is displayed on all pages.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="https://png.icons8.com/dusk/30/000000/paper.png"/>
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
